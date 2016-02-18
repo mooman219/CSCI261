0c9663937c1477d70017fbe6756a6dd453b190ab
Joe Cumbo <mooman219@gmail.com>
2016-02-18 12:26:49 -0500

    Start part1

---
+++ part1/src/MST.java
@@ -0,0 +1,15 @@
+
+/**
+ *
+ * @author Joseph Cumbo (mooman219)
+ */
+public class MST {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
